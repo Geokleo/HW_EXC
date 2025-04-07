75e471925d92202a751520a44e174f441b1b7a22
Geo <geo@nono.com>
2025-04-07 15:47:29 +0300

    Change if statement

--- src/PasswordChecker.java
+++ src/PasswordChecker.java
@@ -22,27 +22,26 @@ public class PasswordChecker {
         if (this.minLength <= 0 || this.maxRepeats <= 0) {
             throw new IllegalStateException("Не установлена Минимальная длина и/или Максимально допустимое количество повторений");
         }
-        if (password.length() >= this.minLength) {
-            if (password.length() == 1) {
-                return true;
-            }
-            int repeats = 1;
-            char lastChar = password.charAt(0);
-            for (int i = 1; i < password.length(); i++) {
-                char currentChar = password.charAt(i);
-                if (currentChar == lastChar) {
-                    repeats++;
-                    if (repeats > this.maxRepeats) {
-                        return false;
-                    }
-                } else {
-                    lastChar = currentChar;
-                    repeats = 1;
-                }
-            }
-            return true;
-        } else {
+        if (password.length() < this.minLength) {
             return false;
         }
+        if (password.length() == 1) {
+            return true;
+        }
+        int repeats = 1;
+        char lastChar = password.charAt(0);
+        for (int i = 1; i < password.length(); i++) {
+            char currentChar = password.charAt(i);
+            if (currentChar == lastChar) {
+                repeats++;
+                if (repeats > this.maxRepeats) {
+                    return false;
+                }
+            } else {
+                lastChar = currentChar;
+                repeats = 1;
+            }
+        }
+        return true;
     }
 }
