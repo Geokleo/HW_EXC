b135a7b85fa79ca983a988db1736fa2b04358a44
Geo <geo@nono.com>
2025-04-07 12:26:20 +0300

    Change if statement

--- src/PasswordChecker.java
+++ src/PasswordChecker.java
@@ -19,31 +19,30 @@ public class PasswordChecker {
     }
 
     public boolean verify(String password) {
-        if (this.minLength > 0 && this.maxRepeats > 0) {
-            if (password.length() >= this.minLength) {
-                if (password.length() == 1) {
-                    return true;
-                }
-                int repeats = 1;
-                char lastChar = password.charAt(0);
-                for (int i = 1; i < password.length(); i++) {
-                    char currentChar = password.charAt(i);
-                    if (currentChar == lastChar) {
-                        repeats++;
-                        if (repeats > this.maxRepeats) {
-                            return false;
-                        }
-                    } else {
-                        lastChar = currentChar;
-                        repeats = 1;
-                    }
-                }
-                return true;
-            } else {
-                return false;
-            }
-        } else {
+        if (this.minLength <= 0 || this.maxRepeats <= 0) {
             throw new IllegalStateException("Не установлена Минимальная длина и/или Максимально допустимое количество повторений");
+        }
+        if (password.length() >= this.minLength) {
+            if (password.length() == 1) {
+                return true;
+            }
+            int repeats = 1;
+            char lastChar = password.charAt(0);
+            for (int i = 1; i < password.length(); i++) {
+                char currentChar = password.charAt(i);
+                if (currentChar == lastChar) {
+                    repeats++;
+                    if (repeats > this.maxRepeats) {
+                        return false;
+                    }
+                } else {
+                    lastChar = currentChar;
+                    repeats = 1;
+                }
+            }
+            return true;
+        } else {
+            return false;
         }
     }
 }
